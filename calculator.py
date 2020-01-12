88d3a0b30955bbd2e2bdd604db1d8833d799c464
David Stansbury <davidmstansbury@gmail.com>
2020-01-12 09:01:50 -0500

    adds three new functions: square, cube and square_n_times. Square and cube only take one parameter (a), but square_n_times takes two parameters (number to be squaredn times, n)

--- calculator.py
+++ calculator.py
@@ -10,6 +10,16 @@ def subtract(a,b):
 def divide(a,b):
 	return a / b
 
+def square(a):
+	return a ** 2
+
+def cube(a):
+	return a ** 3
+
+def square_n_times(num, n):
+	return num ** (2*n)
+
 print("I'm going to use the calculator functions to multiply 5 and 6")
 x = multiply(5, 6)
 print(x)
+
